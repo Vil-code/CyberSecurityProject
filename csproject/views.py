f2fff2e81d3440bffdbad7e2983ae9e5face0e87
Vilhelmi Rintanen <vilhelmi.rintanen@sanoma.com>
2023-04-01 17:53:39 +0300

    add injection vulnerability

--- csproject/views.py
+++ csproject/views.py
@@ -1,3 +1,4 @@
+import html
 from django.contrib.auth.decorators import login_required
 from django.shortcuts import render, redirect
 from django.views.decorators.csrf import csrf_exempt
@@ -20,6 +21,7 @@ def likeView(request):
 @login_required
 def profileView(request):
 	profile = request.POST['profile']
+	# profile = html.escape(profile)
 	print(profile)
 	accounts = Account.objects.exclude(user_id=request.user.id)
 	cur_acc = Account.objects.get(user_id=request.user.id)
